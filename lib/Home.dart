ef20c5992646b5d10f2d4ad27e7a4fd13569b43a
Pedroenrick <pedro_penna22@hotmail.com>
2020-06-11 17:32:18 -0300

    Primeira versão clone youtube

---
+++ lib/Home.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:youtube/customSearchDelegate.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inicio.dart';
+import 'package:youtube/telas/Inscricao.dart';
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _index = 0;
+   String _resultado = "";
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> telas = [
+      Inicio(_resultado),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca()
+      
+    ];
+
+    return Scaffold(
+       appBar: AppBar(
+         iconTheme: IconThemeData(
+           color: Colors.grey,
+         ),
+         backgroundColor: Colors.white,
+         title: Image.asset(
+           "images/youtube.png",
+           width: 98,
+           height: 22,
+          ),
+         actions: <Widget>[
+           IconButton(
+             icon: Icon(Icons.videocam),
+             onPressed: (){
+               print("acao: Video Cam");
+             },
+           ),
+           IconButton(
+             icon: Icon(Icons.search),
+             onPressed: () async{
+               String res = await  showSearch(
+                 context: context, 
+                 delegate: CustomSearchDelegate()
+                );
+                setState(() {
+                  _resultado = res;
+                });
+             },
+           ),
+           IconButton(
+             icon: Icon(Icons.account_circle),
+             onPressed: (){
+               print("acao: Conta");
+             },
+           )
+         ],
+       ),
+       body: Container(
+         padding: EdgeInsets.all(16),
+         child: telas[_index],
+       ),
+       bottomNavigationBar: BottomNavigationBar(
+         type: BottomNavigationBarType.fixed,
+         fixedColor: Colors.red,
+         currentIndex: _index,
+         onTap: (indice){
+           setState(() {
+             _index = indice;
+           });
+         },
+         items: [
+           BottomNavigationBarItem(
+             //backgroundColor: Colors.orange,
+             title: Text("Inicio"),
+             icon: Icon(Icons.home)
+            ),
+           BottomNavigationBarItem(
+             //backgroundColor: Colors.red,
+             title: Text("Em alta"),
+             icon: Icon(Icons.whatshot)
+            ),
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.blue,
+             title: Text("Inscrições"),
+             icon: Icon(Icons.subscriptions)
+            ),
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.green,
+             title: Text("Biblioteca"),
+             icon: Icon(Icons.folder)
+            ),
+         ] 
+        ),
+    );
+  }
+}
